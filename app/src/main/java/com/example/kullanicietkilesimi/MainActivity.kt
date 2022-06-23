50e17d9da87eb3f7e6df6a3951df76f4c64c5680
Mürsel <murselcetin51@gmail.com>
2022-06-23 19:41:02 +0300

    Ders 9 işlendi.

--- app/src/main/java/com/example/kullanicietkilesimi/MainActivity.kt
+++ app/src/main/java/com/example/kullanicietkilesimi/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.kullanicietkilesimi
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.example.kullanicietkilesimi.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -20,8 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonSnackBar.setOnClickListener {
             Snackbar.make(it, "Silmek istiyor musunuz?", Snackbar.LENGTH_LONG)
-                .setAction("Evet"){
-                    Snackbar.make(it,"Silindi",Snackbar.LENGTH_SHORT)
+                .setAction("Evet") {
+                    Snackbar.make(it, "Silindi", Snackbar.LENGTH_SHORT)
                         .setTextColor(Color.BLUE)
                         .setBackgroundTint(Color.WHITE)
                         .show()
@@ -33,7 +35,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonAlert.setOnClickListener {
+            /*val ad = AlertDialog.Builder(this)
+            ad.setTitle("Başlık")
+            ad.setMessage("İçerik")
+            ad.setIcon(R.drawable.resim)
+            ad.setPositiveButton("Tamam") { d, i ->
+                Toast.makeText(this, "Tamam seçildi", Toast.LENGTH_SHORT).show()
+            }
+            ad.setNegativeButton("İptal") { d, i ->
+                Toast.makeText(this, "İptal seçildi", Toast.LENGTH_SHORT).show()
+            }
 
+            ad.create().show()*/
+
+            val ad = AlertDialog.Builder(this)
+            val alertTasarim = layoutInflater.inflate(R.layout.alert_tasarim, null)
+
+            val textViewAlert = alertTasarim.findViewById(R.id.textViewAlert) as TextView
+            textViewAlert.text = "Yapılacaklar"
+
+            val editTextAlert = alertTasarim.findViewById(R.id.editTextAlert) as EditText
+            val checkBoxAlert = alertTasarim.findViewById(R.id.checkBoxAlert) as CheckBox
+            val buttonKaydet = alertTasarim.findViewById(R.id.buttonKaydet) as Button
+            ad.setView(alertTasarim)
+            val d = ad.create()
+
+            buttonKaydet.setOnClickListener {
+                val yapilacakIs = editTextAlert.text.toString()
+                val acilDurum = checkBoxAlert.isChecked
+                Log.e("Alert", "$yapilacakIs - $acilDurum")
+                d.dismiss()
+            }
+            d.show()
         }
     }
 }
